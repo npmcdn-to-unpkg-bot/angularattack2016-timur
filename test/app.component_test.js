4d70881485b37dd68c1887786dd8a845110e9245
Timur Uzel <timurt.uzel@gmail.com>
2016-05-14 19:53:23 -0400

    harder than i thought

---
+++ test/app.component_test.js
@@ -0,0 +1,10 @@
+/**
+ * Created by timur on 5/14/16.
+ */
+var expect = require('chai').expect;
+describe('app', function () {
+    it('should work', function () {
+        expect(true).to.be.false;
+    });
+});
+//# sourceMappingURL=app.component_test.js.map
---
+++ test/app.component_test.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by timur on 5/14/16.
+ */
+
+const expect = require('chai').expect
+
+describe('app', function () {
+    it('should work', function () {
+        expect(true).to.be.false
+    })
+})
