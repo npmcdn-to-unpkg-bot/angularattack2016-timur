fc829e9a2842bbeb7a56a39b893cac4f49a330b0
Timur Uzel <timurt.uzel@gmail.com>
2016-05-14 13:38:48 -0400

    first

--- app/app.component.js
+++ app/app.component.js
@@ -5,6 +5,8 @@
       template: '<h1>AngularAttack 2016</h1><p>Your project runs!</p>'
     })
     .Class({
-      constructor: function() {}
+      constructor: function() {
+        console.log('hello angular 2!')
+      }
     });
 })(window.app || (window.app = {}));
