eb1ded7f0ff77544d5f59b48f8b56558d40f0745
Timur Uzel <timurt.uzel@gmail.com>
2016-05-14 20:53:33 -0400

    not sure what I was thinking

---
+++ karma.conf.js
@@ -0,0 +1,21 @@
+// karma.conf.js
+module.exports = function (config) {
+  config.set({
+    frameworks: ['browserify', 'mocha', 'chai'],
+
+    files: [
+      'test/**/*.js'
+    ],
+
+    reporters: ['nyan'],
+
+    preprocessors: {
+      // source files, that you wanna generate coverage for
+      // do not include tests or libraries
+      // (these files will be instrumented by Istanbul)
+      'test/**/*.js': ['browserify']
+    },
+
+    browsers: ['Chrome']
+  });
+};
--- test/app.component_test.ts
+++ test/app.component_test.ts
@@ -2,7 +2,15 @@
  * Created by timur on 5/14/16.
  */
 
-const expect = require('chai').expect
+const expect = require('chai').expect,
+    assert = require('chai').assert
 
 describe('app', function () {
+
+    const App = require('../app/app.component').AppComponent
+
+    it('should exist', function () {
+        expect(true).to.be.true
+        assert(App)
+    })
 })
