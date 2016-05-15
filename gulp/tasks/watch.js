e7bcb33c8cf9e2282b5ebc58dd5cb10b8457d135
Timur Uzel <timurt.uzel@gmail.com>
2016-05-15 03:16:36 -0400

    typescript sucks

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,10 @@
+/**
+ * Created by timur on 5/14/16.
+ */
+
+const gulp = require('gulp'),
+  paths = require('../paths')
+
+gulp.task('watch', ['build', 'test'], () => {
+  gulp.watch(paths.source, ['test'])
+})
