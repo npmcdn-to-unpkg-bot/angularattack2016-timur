115e89b1a9f4458a927c3652567033d9c5f63b92
Timur Uzel <timurt.uzel@gmail.com>
2016-05-14 20:59:12 -0400

    I hate typescript

---
+++ test/app.component_test.js
@@ -0,0 +1,12 @@
+/**
+ * Created by timur on 5/14/16.
+ */
+var expect = require('chai').expect, assert = require('chai').assert;
+describe('app', function () {
+    var App = require('../app/app.component').AppComponent;
+    it('should exist', function () {
+        expect(true).to.be.true;
+        assert(App);
+    });
+});
+//# sourceMappingURL=app.component_test.js.map
