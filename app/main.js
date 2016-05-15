f85424966920c964ea9cdb648480bcb2a3417204
Timur Uzel <timurt.uzel@gmail.com>
2016-05-14 20:01:20 -0400

    removed js

--- app/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var app_component_1 = require('./app.component');
-var core_1 = require('@angular/core');
-core_1.enableProdMode();
-platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent);
-//# sourceMappingURL=main.js.map
